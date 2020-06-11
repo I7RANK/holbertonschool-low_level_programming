6b28da186a1b8e035dfd8b151504bc50c1ee23ee
I7RANK <franfgh2000@hotmail.com>
2020-06-11 23:17:14 +0000

    Fix style betty of 102.

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,13 +14,10 @@ for (i = 48; i <= 57; i++)
 {
 for (a = 48; a <= 56; a++)
 {
-
-
 for (b = i; b <= 57; b++)
 {
 for (c = 48; c <= 57; c++)
 {
-
 putchar(i);
 putchar(a);
 
@@ -28,13 +25,10 @@ putchar(32);
 
 putchar(b);
 
-
 if (i == b)
 {
-
 if (c <= a)
 c = a + 1;
-
 }
 
 putchar(c);
@@ -48,14 +42,10 @@ else
 putchar(44);
 putchar(32);
 }
-
 }
 }
-
-
 }
 }
-
 
 return (0);
 }

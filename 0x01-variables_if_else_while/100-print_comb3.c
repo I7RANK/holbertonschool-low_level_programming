b48af048e27897aca4aa8e73963234c08908014e
I7RANK <franfgh2000@hotmail.com>
2020-06-15 20:55:41 +0000

    format code

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,24 +8,25 @@
  */
 int main(void)
 {
-int i, a;
-for (i = 48; i <= 56; i++)
-{
-for (a = i + 1; a <= 57; a++)
-{
-putchar(i);
-putchar(a);
-if (i == 56 && a == 57)
-{
-putchar(10);
-}
-else
-{
-putchar(44);
-putchar(32);
-}
-}
-}
+	int i, a;
 
-return (0);
+	for (i = 48; i <= 56; i++)
+	{
+		for (a = i + 1; a <= 57; a++)
+		{
+			putchar(i);
+			putchar(a);
+			if (i == 56 && a == 57)
+			{
+				putchar(10);
+			}
+			else
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+	}
+
+	return (0);
 }
--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,27 +8,29 @@
  */
 int main(void)
 {
-int i, a, b;
-for (i = 48; i <= 55; i++)
-{
-for (a = i + 1; a <= 56; a++)
-{
-for (b = a + 1; b <= 57; b++)
-{
-putchar(i);
-putchar(a);
-putchar(b);
-if (i == 55 && a == 56 && b == 57)
-{
-putchar(10);
-}
-else
-{
-putchar(44);
-putchar(32);
-}
-}
-}
-}
-return (0);
+	int i, a, b;
+
+	for (i = 48; i <= 55; i++)
+	{
+		for (a = i + 1; a <= 56; a++)
+		{
+			for (b = a + 1; b <= 57; b++)
+			{
+				putchar(i);
+				putchar(a);
+				putchar(b);
+				if (i == 55 && a == 56 && b == 57)
+				{
+					putchar(10);
+				}
+				else
+				{
+					putchar(44);
+					putchar(32);
+				}
+			}
+		}
+	}
+
+	return (0);
 }

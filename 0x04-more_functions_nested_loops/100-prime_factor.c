4da7e17552e6fcbf5d274259078ceaae5758a30a
I7RANK <franfgh2000@hotmail.com>
2020-06-18 22:16:57 +0000

    fix task 100.

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- *
- *
- *
+ * main - prints the largest prime factor of the number 612852475143
+ * Description: followed by a new line
+ * Return: return 0
  */
 int main(void)
 {
@@ -15,10 +15,11 @@ int main(void)
 		{
 			if (n % i == 0)
 			{
-				n = n / i;
-				i = n + 1;
 				if (i >= fact)
 					fact = i;
+
+				n = n / i;
+				i = n + 1;
 			}
 		}
 	}

dc1f8d8c69fe7770750d846ac4be0d85ed69e798
I7RANK <franfgh2000@hotmail.com>
2020-06-26 02:01:36 +0000

    ws

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -12,26 +12,16 @@
  */
 char *leet(char *a)
 {
-	char replace[10][2] = {
-		{'a', '4'},
-		{'A', '4'},
-		{'e', '3'},
-		{'E', '3'},
-		{'o', '0'},
-		{'O', '0'},
-		{'t', '7'},
-		{'T', '7'},
-		{'l', '1'},
-		{'L', '1'}
-	};
+	char letter[] = "aAeEoOtTlL";
+	char replace[] = "43071";
 	int i, b;
 
 	while (a[i] != '\0')
 	{
 		for (b = 0; b < 10; b++)
 		{
-			if (a[i] == replace[b][0])
-				a[i] = replace[b][1];
+			if (a[i] == letter[b])
+				a[i] = replace[(b / 2)];
 		}
 		i++;
 	}

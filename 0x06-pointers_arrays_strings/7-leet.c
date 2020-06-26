7b93d97f8506be9c97e9f87eecd11390209bf1b3
I7RANK <franfgh2000@hotmail.com>
2020-06-26 02:12:11 +0000

    copy

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,23 +7,36 @@
  * Letters o and O should be replaced by 0
  * Letters t and T should be replaced by 7
  * Letters l and L should be replaced by 1
- * @a: is the array
+ * @s1: is the array
  * Return: return the pointer
  */
-char *leet(char *a)
+char *leet(char *s1)
 {
-	char letter[] = "aAeEoOtTlL";
-	char replace[] = "4433007711";
-	int i, b;
-
-	while (a[i] != '\0')
+	int i, j;
+	char code[10][2] = {
+		{'a', '4'},
+		{'A', '4'},
+		{'e', '3'},
+		{'E', '3'},
+		{'o', '0'},
+		{'O', '0'},
+		{'t', '7'},
+		{'T', '7'},
+		{'l', '1'},
+		{'L', '1'}
+	};
+	for (i = 0; s1[i]; i++)
 	{
-		for (b = 0; b < 10; b++)
+		for (j = 0; code[j][0]; j++)
 		{
-			if (a[i] == letter[b])
-				a[i] = replace[b];
+			if (s1[i] == code[j][0])
+			{
+				s1[i] = code[j][1];
+			}
+
 		}
-		i++;
+
 	}
-	return (a);
+
+	return (s1);
 }

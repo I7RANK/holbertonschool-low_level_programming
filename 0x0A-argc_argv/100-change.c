ae1aa15c119aeb3e77c84982cf0ab8bd044c74c9
I7RANK <franfgh2000@hotmail.com>
2020-07-04 23:38:52 +0000

    fix betty 100.

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -23,14 +23,7 @@ int main(int argc, char *argv[])
 	}
 
 	cents = atoi(argv[1]);
-
-	if (cents <= 0)
-	{
-                printf("0\n");
-                return (0);
-        }
-
-	while (cents != 0)
+	while (cents > 0)
 	{
 		if (cents >= 25)
 		{

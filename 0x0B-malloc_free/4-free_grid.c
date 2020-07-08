5fda61004ad4e70d18105bd4925b565adf23e5d7
I7RANK <franfgh2000@hotmail.com>
2020-07-08 23:06:55 +0000

    uploading task 4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - frees a 2 dimensional grid
+ * @grid: the grid to free
+ * @height: is a heght of the grid
+ * Return: no return
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+
+	free(grid);
+}

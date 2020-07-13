1606e1aa9b9a694d95d192afbe581f916349f589
I7RANK <franfgh2000@hotmail.com>
2020-07-13 23:44:13 +0000

    up 4(after test)

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -17,7 +17,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (new_dog == NULL)
 		return (NULL);
 
-	/*new_dog->name = malloc(sizeof(name) + 1);
+	new_dog->name = malloc(sizeof(name) + 1);
 	if (new_dog->name == NULL)
 	{
 		free(new_dog);
@@ -30,7 +30,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(new_dog->name);
 		free(new_dog);
 		return (NULL);
-		}*/
+	}
 
 	for (i = 0; name[i]; i++)
 		new_dog->name[i] = name[i];

3cd80ddf956ca5e3b783cf284cbdaafa1f379947
I7RANK <franfgh2000@hotmail.com>
2020-07-13 22:42:41 +0000

    fix 1 and 2

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,11 +2,16 @@
 
 /**
  * init_dog - initializes a variable of type struct dog
- *
+ * @d: is the strcture
+ * @name: is the name
+ * @age: is the age
+ * @owner: is the owner
  * Return: 0
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+		return (NULL);
 	d->name = name;
 	d->age = age;
 	d->owner = owner;

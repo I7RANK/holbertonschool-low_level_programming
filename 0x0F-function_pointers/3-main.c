c5a9c0e33800eddb0d0f76171c8bbe12753ac34b
I7RANK <franfgh2000@hotmail.com>
2020-07-15 22:31:21 +0000

    fix 3 attempt 2

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -12,10 +12,8 @@
  */
 int main(int argc, char *argv[])
 {
-	int calc, num1, num2;
+	int num1, num2;
 	char *operator;
-
-	int (*calc_func)(int, int);
 
 	if (argc != 4)
 	{
@@ -27,8 +25,8 @@ int main(int argc, char *argv[])
 	operator = argv[2];
 	num2 = atoi(argv[3]);
 
-	if (*operator != '+' && *operator != '-' && *operator != '*' &&
-	    *operator != '/' && *operator != '%')
+	if ((*operator != '+' && *operator != '-' && *operator != '*' &&
+	     *operator != '/' && *operator != '%') || operator[1])
 	{
 		printf("Error\n");
 		return (99);
@@ -40,10 +38,6 @@ int main(int argc, char *argv[])
 		return (100);
 	}
 
-	calc_func = get_op_func(operator);
-
-	calc = calc_func(num1, num2);
-
-	printf("%d\n", calc);
+	printf("%d\n", get_op_func(operator)(num1, num2));
 	return (0);
 }

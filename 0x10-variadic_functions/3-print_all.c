92284c6b84d7db7fb4b9f89bdab624d250802f8f
I7RANK <franfgh2000@hotmail.com>
2020-07-18 20:51:29 +0000

    test with switch

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,10 +1,5 @@
 #include "variadic_functions.h"
 
-void print_c(va_list copy);
-void print_i(va_list copy);
-void print_f(va_list copy);
-void print_s(va_list copy);
-
 /**
  * print_all - prints anything, followed by a new line
  * @format: is the format for know that print
@@ -13,80 +8,37 @@ void print_s(va_list copy);
 void print_all(const char * const format, ...)
 {
 	va_list valist;
-	int i = 0, j;
-	char *sepa = "";
-
-	select_p select[] = {
-		{"c", print_c},
-		{"i", print_i},
-		{"f", print_f},
-		{"s", print_s}
-	};
+	int i = 0;
+	char *separa = ", ", *s;
 
 	va_start(valist, format);
 
 	while (format[i])
 	{
-		j = 0;
-		while (j <= 3)
+		if (format[i + 1] == '\0')
+			separa = "";
+
+		switch (format[i])
 		{
-			if (format[i] == select[j].f[0])
-			{
-				printf("%s", sepa);
-				(select[j].p)(valist);
-				sepa = ", ";
-				break;
-			}
-			j++;
+		case 'c':
+			printf("%c%s", va_arg(valist, int), separa);
+			break;
+		case 'i':
+			printf("%d%s", va_arg(valist, int), separa);
+			break;
+		case 'f':
+			printf("%f%s", va_arg(valist, double), separa);
+			break;
+		case 's':
+			s = va_arg(valist, char *);
+			if (*s == '\0')
+				printf("(nil)%s", separa);
+			else
+				printf("%s%s", s, separa);
+			break;
 		}
 		i++;
 	}
-
-	va_end(valist);
 	printf("\n");
-}
-
-/**
- * print_c - prints a character
- * @copy: is the va_list variable
- * Return: na
- */
-void print_c(va_list copy)
-{
-	printf("%c", va_arg(copy, int));
-}
-
-/**
- * print_i - prints a int
- * @copy: is the va_list variable
- * Return: na
- */
-void print_i(va_list copy)
-{
-	printf("%d", va_arg(copy, int));
-}
-
-/**
- * print_f - prints a float
- * @copy: is the va_list variable
- * Return: na
- */
-void print_f(va_list copy)
-{
-	printf("%f", va_arg(copy, double));
-}
-
-/**
- * print_s - prints a string
- * @copy: is the va_list variable
- * Return: na
- */
-void print_s(va_list copy)
-{
-	char *va = va_arg(copy, char *);
-
-	if (va == NULL)
-		va = "(nil)";
-
-	printf("%s", va);
+	va_end(valist);
 }

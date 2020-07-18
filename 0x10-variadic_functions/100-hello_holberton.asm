449474e24ab9dc02ea846592c15efb2db2f2c0bf
I7RANK <franfgh2000@hotmail.com>
2020-07-18 23:00:44 +0000

    fix 100 rare

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,11 +1,15 @@
-global  _main
-	    extern  _printf
+section .data
 
-	    section .text
-_main:
-	    push    message
-	    syscall    _printf
-	    add     esp, 4
-	    ret
-message:
-	    db  'Hello, World', 10, 0
+	message db "Hello, Holberton", 10
+
+	section .text
+	global main
+main:
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, message
+	mov rdx, 17
+	syscall
+	mov    rax, 60
+	mov    rdi, 0
+	syscall

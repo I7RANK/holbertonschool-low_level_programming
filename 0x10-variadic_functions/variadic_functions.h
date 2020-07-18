e55bc46d03fb8cfbc62b7d1f531fa2c08663e176
I7RANK <franfgh2000@hotmail.com>
2020-07-18 19:30:09 +0000

    Rename directory and add _putchar

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -14,6 +14,7 @@ typedef struct print
 	void (*p)();
 } select_p;
 
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);

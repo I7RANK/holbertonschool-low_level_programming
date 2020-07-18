ab0e537c1ee4a7666b9f7806a51b401094f06b67
I7RANK <franfgh2000@hotmail.com>
2020-07-18 21:15:37 +0000

    return to 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,5 +1,10 @@
 #include "variadic_functions.h"
 
+void print_c(va_list copy);
+void print_i(va_list copy);
+void print_f(va_list copy);
+void print_s(va_list copy);
+
 /**
  * print_all - prints anything, followed by a new line
  * @format: is the format for know that print
@@ -8,35 +13,81 @@
 void print_all(const char * const format, ...)
 {
 	va_list valist;
-	int i = 0;
-	char *separa = "", *s;
+	int i = 0, j;
+	char *sepa = "";
+
+	select_p select[] = {
+		{"c", print_c},
+		{"i", print_i},
+		{"f", print_f},
+		{"s", print_s}
+	};
 
 	va_start(valist, format);
 
 	while (format[i])
 	{
-		switch (format[i])
+		j = 0;
+		while (j <= 3)
 		{
-		case 'c':
-			printf("%s%c", separa, va_arg(valist, int));
-			break;
-		case 'i':
-			printf("%s%d", separa, va_arg(valist, int));
-			break;
-		case 'f':
-			printf("%s%f", separa, va_arg(valist, double));
-			break;
-		case 's':
-			s = va_arg(valist, char *);
-			if (s == NULL)
-				s = "(nil)";
-			printf("%s%s", separa, s);
-			break;
+			if (format[i] == select[j].f[0])
+			{
+				printf("%s", sepa);
+				(select[j].p)(valist);
+				sepa = ", ";
+			}
+			j++;
 		}
-		separa = ", ";
 		i++;
 	}
 
 	va_end(valist);
 	printf("\n");
+}
+
+/**
+ * print_c - prints a character
+ * @copy: is the va_list variable
+ * Return: na
+ */
+void print_c(va_list copy)
+{
+	printf("%c", va_arg(copy, int));
+}
+
+/**
+ * print_i - prints a int
+ * @copy: is the va_list variable
+ * Return: na
+ */
+void print_i(va_list copy)
+{
+	printf("%d", va_arg(copy, int));
+}
+
+/**
+ * print_f - prints a float
+ * @copy: is the va_list variable
+ * Return: na
+ */
+void print_f(va_list copy)
+{
+	printf("%f", va_arg(copy, double));
+}
+
+/**
+ * print_s - prints a string
+ * @copy: is the va_list variable
+ * Return: na
+ */
+void print_s(va_list copy)
+{
+	char *va = va_arg(copy, char *);
+
+	if (va == NULL)
+	{
+		printf("(nil)");
+		return;
+	}
+	printf("%s", va);
 }

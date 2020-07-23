987d7f619d8d915bbef78108e989be6110d97568
I7RANK <franfgh2000@hotmail.com>
2020-07-23 23:05:27 +0000

    fix 100

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,7 +5,7 @@
  *
  * Return: na
  */
-void Ifirst(void) __attribute__ ((constructor))
+void __attribute__((constructor))Ifirst(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");

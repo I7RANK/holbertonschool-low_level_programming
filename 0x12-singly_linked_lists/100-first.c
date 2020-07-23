bf6b7f295b7ded082904373fe18d06e05c4094cc
I7RANK <franfgh2000@hotmail.com>
2020-07-23 21:51:27 +0000

    uploading task 100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * Ifirst - prints a message before to tehe main function
+ *
+ * Return: na
+ */
+void Ifirst(void) __attribute__ ((constructor))
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}

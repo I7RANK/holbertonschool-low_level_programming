60859533629b8647e31dda7efce287ec74ad8b9c
I7RANK <franfgh2000@hotmail.com>
2020-07-28 15:46:40 +0000

    fix validation

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,11 +10,9 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	if (*head == NULL)
-	{
-		head = NULL;
+	if (head == NULL)
 		return;
-	}
+
 	while (*head)
 	{
 		temp = (*head)->next;

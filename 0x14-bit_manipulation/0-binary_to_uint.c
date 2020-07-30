69cd9310e1fb4ed3845f2ffce14a8dc17fdd79a6
I7RANK <franfgh2000@hotmail.com>
2020-07-30 19:37:49 +0000

    uploading first task

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * binary_to_uint - coverts a binary number to an usigned int
+ * @b: the string of number binary
+ *
+ * Return: the converted number or 0
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int finder = 1, num = 0;
+	int i;
+
+	for (i = 0; b[i]; i++)
+	{}
+
+	i--;
+	for (; i >= 0; i--, finder *= 2)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+
+		if (b[i] == '1')
+			num += finder;
+	}
+
+	return (num);
+}

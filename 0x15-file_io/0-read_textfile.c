f0f3884bc76ff32911c7714fb0f9afac0115a119
I7RANK <franfgh2000@hotmail.com>
2020-08-04 18:38:35 +0000

    uploading task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+
+/**
+ * read_textfile - reads and writes a file
+ * @filename: it's the name of the file
+ * @letters: is the number of letters it should read and print
+ *
+ * Return: the number of letters reads and prints
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd = 0, cant_rd = 0, cant_wr = 0;
+	char *fd_read;
+
+	if (filename == NULL)
+		return (0);
+
+	fd_read = malloc(sizeof(char) * letters);
+	if (fd_read == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	cant_rd = read(fd, fd_read, letters);
+	if (cant_rd < 0)
+		return (0);
+
+	cant_wr = write(STDOUT_FILENO, fd_read, letters);
+	if (cant_wr < 0)
+		return (0);
+
+	close(fd);
+	free(fd_read);
+
+	return (cant_wr);
+}

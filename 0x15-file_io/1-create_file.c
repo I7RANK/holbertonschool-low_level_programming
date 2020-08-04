c174b05e8cd677c7e1ffc7f12ee8cbcfc28c5143
I7RANK <franfgh2000@hotmail.com>
2020-08-04 20:16:59 +0000

    add the if

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,8 +18,9 @@ int create_file(const char *filename, char *text_content)
 	if (fd < 0)
 		return (-1);
 
-	for (len = 0; text_content[len]; len++)
-	{}
+	if (text_content)
+		for (len = 0; text_content[len]; len++)
+		{}
 
 	lwr = write(fd, text_content, len);
 	if (lwr < 0)

4db0940753d358181f7b16c6af643632629ead42
I7RANK <franfgh2000@hotmail.com>
2020-08-04 20:21:56 +0000

    fix if

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,8 +19,10 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (text_content)
+	{
 		for (len = 0; text_content[len]; len++)
 		{}
+	}
 
 	lwr = write(fd, text_content, len);
 	if (lwr < 0)

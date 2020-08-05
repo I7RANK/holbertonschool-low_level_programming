9be901d74c8e77535f5dcf4a7cca268e51041665
I7RANK <franfgh2000@hotmail.com>
2020-08-05 01:17:52 +0000

    change 2 for macro and conditional while

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,30 +16,30 @@ int main(int ac, char **av)
 
 	if (ac != 3)
 	{
-		dprintf(2, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		return (97);
 	}
 
 	f1 = open(av[1], O_RDONLY);
 	if (f1 < 0)
 	{
-		dprintf(2, "Error: Can't read from file %s", av[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s", av[1]);
 		return (98);
 	}
 
 	f2 = open(av[2], O_RDWR | O_CREAT | O_TRUNC, 0664);
 	if (f2 < 0)
 	{
-		dprintf(2, "Error: Can't write to %s", av[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s", av[2]);
 		return (99);
 	}
 
-	while (len1 != 0)
+	while (len1)
 	{
 		len1 = read(f1, buff, 1024);
 		if (len1 < 0)
 		{
-			dprintf(2, "Error: Can't read from file %s", av[1]);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s", av[1]);
 			return (98);
 		}
 
@@ -48,7 +48,7 @@ int main(int ac, char **av)
 			lwr = write(f2, buff, len1);
 			if (lwr < 0)
 			{
-				dprintf(2, "Error: Can't write to %s", av[2]);
+				dprintf(STDERR_FILENO, "Error: Can't write to %s", av[2]);
 				return (99);
 			}
 		}
@@ -70,7 +70,7 @@ void _close(int fd)
 {
 	if (close(fd) < 0)
 	{
-		dprintf(2, "Error: Can't close fd %d\n", fd);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
 	}
 }

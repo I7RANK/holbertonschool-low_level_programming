ed2835f7668b9e208336311a0bd0c6cb6459aa54
I7RANK <franfgh2000@hotmail.com>
2020-11-06 19:55:30 -0500

    fix other case

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -23,7 +23,9 @@ def island_perimeter(grid):
             if grid[i][j] == 1:
                 """ up """
                 try:
-                    if grid[i-1][j] == 0:
+                    if (i - 1) < 0:
+                        perimeter += 1
+                    elif grid[i-1][j] == 0:
                         perimeter += 1
                 except IndexError:
                     perimeter += 1
